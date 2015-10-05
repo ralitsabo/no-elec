fc8f5b782301bf259e7fd97ab6686064cb0a68e6
Ralitsa Borisova <ralitsa@empowerunited.com>
2015-10-05 16:53:39 +0300

    validate

--- routes/index.js
+++ routes/index.js
@@ -17,35 +17,6 @@ router.get('/userlist', function(req, res) {
   });
 });
 
-/* POST to Add User Service */
-router.post('/adduser', function(req, res) {
-
-    // Set our internal DB variable
-    var db = req.db;
-
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-
-    // Set our collection
-    var collection = db.get('usercollection');
-
-    // Submit to the DB
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // And forward to success page
-            res.redirect("userlist");
-        }
-    });
-});
-
 /* GET New User page. */
 router.get('/newuser', function(req, res) {
     res.render('newuser', { title: 'Add New User' });
@@ -60,6 +31,14 @@ router.post('/adduser', function(req, res) {
     // Get our form values. These rely on the "name" attributes
     var userName = req.body.username;
     var userEmail = req.body.useremail;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (!userEmail.match(mailformat)) {
+      return true;
+    }
+    else {
+      console.log('enter normal email');
+    }
 
     // Set our collection
     var collection = db.get('usercollection');
